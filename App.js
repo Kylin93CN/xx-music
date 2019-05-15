7b69114c935216dfed74d94fe698555101d5f47f
Kylin93CN <375020612@qq.com>
2019-05-15 22:55:53 +0800

    播放音视频

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React, {Component} from 'react';
+import Video from 'react-native-video';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
@@ -21,15 +22,35 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+        <Video
+          source={{uri: "https://ugc.shangzhibo.tv/88ec701969401ca34fe22ba25956b5ae.mp3"}}   // Can be a URL or a local file.
+          ref={(ref) => {
+            this.player = ref
+          }}                                      // Store reference
+          // poster="https://ugc.shangzhibo.tv/0cebae7fbb174c843108a3ac4c6b9a2a.gif"
+          audioOnly
+          playInBackground
+          controls
+          ignoreSilentSwitch="ignore"
+          onBuffer={this.onBuffer}                // Callback when remote video is buffering
+          onError={this.videoError}               // Callback when video cannot be loaded
+          style={styles.backgroundVideo} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
